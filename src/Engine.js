d8eb753e461e150c08acbb0f52a42180ffc5b1b7
kvasseur <vasseur.kev@outlook.fr>
2016-10-11 13:48:11 +0200

    quatrieme histoire

--- src/Engine.js
+++ src/Engine.js
@@ -13,39 +13,77 @@ var Engine =  {
         Engine.board[1] = ["yellow", "white", "green", "red", "yellow", "blue"];
         Engine.board[2] = ["blue", "yellow", "blue", "white", "black", "red"];
         Engine.board[3] = ["red", "black", "red", "green", "blue", "white"];
-        Engine.board[4] =["white", "green", "yellow", "black", "yellow", "green"];
+        Engine.board[4] = ["white", "green", "yellow", "black", "yellow", "green"];
         Engine.board[5] = ["yellow", "blue", "black", "red", "green", "black"];
-            
-        
 
         return Engine.board;
     },
 
+    change_player:function(){
 
-    play:function () {
+        if(Engine.current_player == 1){
+            Engine.current_player = 2;
+        }
 
-        Engine.tab_player1.push("yellow");
+        else{
+            Engine.current_player = 1;
+        }
+
+    },
+
+
+    play:function (coordinates) {
+        
+
+        var x = coordinates.charCodeAt(0) - 65;
+        var y = coordinates.charAt(1) - 1;
+
+        var couleur = Engine.board[y][x];
+
+        if(Engine.current_player == 1 && Engine.board[y][x]!= null) {
+
+            Engine.tab_player1.push(couleur);
+            
+
+        }
+
+        if(Engine.current_player == 2 && Engine.board[y][x]!= null) {
+
+            Engine.tab_player2.push(couleur);
+            
+        }
+        
 
         return Engine.tab_player1[0];
 
     },
 
-    remove:function () {
+    remove:function (coordinates) {
         
+
+        var x = coordinates.charCodeAt(0) - 65;
+        var y = coordinates.charAt(1) - 1;
+        
+        Engine.board[y][x] = null;
+        
+        
+    },
+    
+    nb_couleurs:function () {
         var compteur = 0;
         
-        Engine.board[5][0] = null;
-        
         for(var i=0; i<6; i++){
-            for(var j=0; j<6; j++){
+            for(var j=0; j<7; j++){
                 if(Engine.board[i][j] != null){
                     compteur++;
                 }
             }
         }
-        
+
         return compteur;
     }
+
+
 
 
 
--- test/test.js
+++ test/test.js
@@ -18,14 +18,53 @@ PalettoTestCase.prototype.testStory1 = function () {
 
 PalettoTestCase.prototype.testStory2 = function () {
 
-    var test = Engine.play();
+    var test = Engine.play("A6");
+    Engine.remove("A6");
+    
+    var nb = Engine.nb_couleurs();
+    
     assertEquals(test, "yellow");
-
+    assertEquals(nb, 35);
+    
+    
 };
 
 PalettoTestCase.prototype.testStory3 = function () {
 
-    var test = Engine.remove();
-    assertEquals(test, 35);
+    Engine.remove("A6");
+    var nb = Engine.nb_couleurs();
+    assertEquals(nb, 35);
+
+};
+
+PalettoTestCase.prototype.testStory4 = function () {
+    Engine.change_player();
+    
+    Engine.play("A1");
+    Engine.play("F6");
+    
+    Engine.remove("A1");
+    Engine.remove("F6");
+
+    var test = Engine.nb_couleurs();
+    /*for(var i = 0; i<10; i++){
+        console.log(Engine.tab_player1[i]);
+    }
+
+    for(var j = 0; j<10; j++){
+        console.log(Engine.tab_player2[j]);
+    }*/
+
+    /*for(var i=0; i<6; i++){
+        for(var j=0; j<6; j++){
+            if(Engine.board[i][j] != null){
+                console.log(Engine.board[i][j]);
+            }
+        }
+    }*/
+    
+    var nb = Engine.nb_couleurs();
+
+    assertEquals(nb, 33);
 
 };
