5de280cc6f908824eb6da6c151f58b2d3e6f061e
kvasseur <vasseur.kev@outlook.fr>
2016-10-11 11:12:31 +0200

    premiere histoire

--- src/Engine.js
+++ src/Engine.js
@@ -1,8 +1,21 @@
 'use strict';
 
-var Engine = function () {
+var Engine =  {
 
-// private attributes and methods
+    init_game:function () {
 
-// public methods
+        var plateau = [
+            ["black", "green", "white", "blue", "red", "white"],
+            ["yellow", "white", "green", "red", "yellow", "blue"],
+            ["blue", "yellow", "blue", "white", "black", "red"],
+            ["red", "black", "red", "green", "blue", "white"],
+            ["white", "green", "yellow", "black", "yellow", "green"],
+            ["yellow", "blue", "black", "red", "green", "black"]
+        ]
+
+        return plateau;
+    },
+
+
+
 };
--- test/test.js
+++ test/test.js
@@ -3,4 +3,15 @@
 var PalettoTestCase = TestCase("PalettoTestCase");
 
 PalettoTestCase.prototype.testStory1 = function () {
+    
+    var tab = Engine.init_game();
+
+        
+    assertEquals(tab[0], ["black", "green", "white", "blue", "red", "white"]);
+    assertEquals(tab[1], ["yellow", "white", "green", "red", "yellow", "blue"]);
+    assertEquals(tab[2], ["blue", "yellow", "blue", "white", "black", "red"]);
+    assertEquals(tab[3], ["red", "black", "red", "green", "blue", "white"]);
+    assertEquals(tab[4],  ["white", "green", "yellow", "black", "yellow", "green"]);
+    assertEquals(tab[5],  ["yellow", "blue", "black", "red", "green", "black"]);
+    
 };
